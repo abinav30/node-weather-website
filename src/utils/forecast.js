321d6abf8185ff83bfec586d1c36f380eca817f7
Abinav Murugadass <abinav.murugadass@gmail.com>
2019-07-17 18:06:26 -0400

    added an hourly summary in the search results

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,15 @@
 const request=require("request");
 const forecast=(latitude,longitude,callback)=>{
-    const url=`https://api.darksky.net/forecast/b41dccf2fad74917d851113e608181b9/${latitude},${longitude}?exclude=minutely,hourly&units=si`;
+    const url=`https://api.darksky.net/forecast/b41dccf2fad74917d851113e608181b9/${latitude},${longitude}?exclude=minutely&units=si`;
     request({url:url, json:true},(error,response)=>{
         try{
             if(response.body.error){
                 callback(`Bad Request ${response.body.error}`);
             }
             else{
-                let {daily,currently}=response.body;
+                let {daily,currently,hourly}=response.body;
                 let data=daily.data[0].summary +"\n"+ "It is currently "+ currently.temperature+ " degree celcius outside and chances of rain are "+ currently.precipProbability+ " %";
+                data =data + " " + hourly.summary;
              callback(undefined, data);
             }
              //console.log(response.body);
